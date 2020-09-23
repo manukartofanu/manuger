6d20210b32d0b236e3bb218aac6e5f3adcdd7327
manukartofanu <manukartofanu@gmail.com>
2020-09-23 04:04:09 +0300

    Add sqlite database

---
+++ wpf/src/Manuger.Core/SqliteDataAccess.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+
+namespace Manuger.Core
+{
+	public class SqliteDataAccess
+	{
+		public static List<Team> LoadTeams()
+		{
+			using (IDbConnection connection = new SQLiteConnection(LoadConnectionString()))
+			{
+				var output = connection.Query<Team>("select * from Team", new DynamicParameters());
+				return output.ToList();
+			}
+		}
+
+		public static void SaveTeam(Team team)
+		{
+			using (IDbConnection connection = new SQLiteConnection(LoadConnectionString()))
+			{
+				connection.Execute("insert into Person (Name) values (@Name)", team);
+			}
+		}
+
+		private static string LoadConnectionString(string id = "Default")
+		{
+			return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+		}
+	}
+}
---
+++ wpf/src/Manuger.Core/Team.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Manuger.Core
+{
+	public class Team
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+	}
+}
