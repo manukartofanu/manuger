7440aa1a16fa8f89c64a931697da0f22440e130b
manukartofanu <manukartofanu@gmail.com>
2021-09-16 19:09:39 +0800

    Refactor database interfaces

--- wpf/src/Manuger.Core/Database/IRepository.cs
+++ wpf/src/Manuger.Core/Database/IRepository.cs
@@ -3,11 +3,9 @@ using System.Collections.Generic;
 
 namespace Manuger.Core.Database
 {
-	public interface IRepository<T> : IDisposable
+	public interface IRepository<T> : IReadRepository<T>
 		where T : class
 	{
-		IEnumerable<T> GetAllItems();
-		T GetItem(long id);
 		void CreateItem(T item);
 	}
 }
