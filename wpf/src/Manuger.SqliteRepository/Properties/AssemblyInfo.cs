4409b7d2640ded819c56cc478552a9fca48db41d
manukartofanu <manukartofanu@gmail.com>
2022-04-10 19:30:29 +0200

    Move sqlite repository

--- wpf/src/Manuger.SqliteRepository/Properties/AssemblyInfo.cs
+++ wpf/src/Manuger.SqliteRepository/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("cbd8c519-bf1e-4bd8-802c-0ed5a011471d")]
+[assembly: Guid("931065d5-1483-4174-9e27-439783606e92")]
 
 // Version information for an assembly consists of the following four values:
 //
